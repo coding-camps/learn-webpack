0b506390d01d083d3175b38f173e8e2270911eb2
JacobCN <coder211@163.com>
2022-02-23 09:22:55 +0800

    update indent with black space and reformat code style

--- j-cli/webpack.prod.js
+++ j-cli/webpack.prod.js
@@ -5,29 +5,29 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = merge(base, {
     mode: "production",
-    devtool:'source-map',//独立配置源码映射
+    devtool: 'source-map',//独立配置源码映射
     module: {
         rules: [
-			{ //用来编译css代码 // loader 执行顺序 自下而上
-				test:/\.css$/,
-				use:[
+            { //用来编译css代码 // loader 执行顺序 自下而上
+                test: /\.css$/,
+                use: [
                     //抽取css样式文件
-					MiniCssExtractPlugin.loader,
-					{loader:'css-loader'},
+                    MiniCssExtractPlugin.loader,
+                    {loader: 'css-loader'},
                     // 解决样式兼容性
-					{loader:'postcss-loader'},
-				]
-			},
-			{ //用来编译sass代码
-				test:/\.scss$/,
-				use:[
+                    {loader: 'postcss-loader'},
+                ]
+            },
+            { //用来编译sass代码
+                test: /\.scss$/,
+                use: [
                     //抽取css样式文件
-					MiniCssExtractPlugin.loader,
-					{loader:'css-loader'},
-					{loader:'postcss-loader'},
-					{loader:'sass-loader'}
-				]
-			}
+                    MiniCssExtractPlugin.loader,
+                    {loader: 'css-loader'},
+                    {loader: 'postcss-loader'},
+                    {loader: 'sass-loader'}
+                ]
+            }
         ],
     },
     output: {
